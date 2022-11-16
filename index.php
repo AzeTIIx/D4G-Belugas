8f61a5fcaf199bfc19f1b168bd0a3a7f570d9ac1
Charles AIMIN <caimin.ing2024@esaip.org>
2022-11-16 13:20:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,11 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Les BELUGAS</title>
 </head>
 <body>
+<header> <?php require_once("php/header.php");?> </header>
 
-<header>
-
-
-</header>
 
 <main>
 
@@ -21,7 +18,7 @@
 
 </main>
 
-<footer></footer>
+<footer><?php require_once("php/footer.php");?></footer>
   
 </body>
 </html>
